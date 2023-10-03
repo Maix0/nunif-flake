c736b36b351f8ff8c2c829c341ab72423b339122
Maix0 <maix522@gmail.com>
2023-10-03 20:43:44 +0200

    probably a non-working version

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Waifu2x's flake setup";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.waifu2x-src.url = "github:nagadomi/nunif";
+  inputs.waifu2x-src.flake = false;
+
+  inputs.onnx-model.url = "https://github.com/nagadomi/nunif/releases/download/0.0.0/waifu2x_onnx_models_20230504.zip";
+  inputs.onnx-model.flake = false;
+  inputs.pytorch-model.url = "https://github.com/nagadomi/nunif/releases/download/0.0.0/waifu2x_pretrained_models_20230504.zip";
+  inputs.pytorch-model.flake = false;
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    waifu2x-src,
+    onnx-model,
+    pytorch-model,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        waifu2x = pkgs.stdenv.mkDerivation {
+          srcs = [waifu2x-src onnx-model pytorch-model];
+          setSourceRoot = ''
+            export sourceRoot="."
+            ${pkgs.tree}/bin/tree -ifFC .
+          '';
+          InstallPhase = ''
+              
+
+          '';
+          nativeBuildInputs = [pkgs.unzip];
+        };
+      in {
+        devShell = let
+          my-pkgs = ps: [
+            ps.torchvision
+            ps.pytorch
+            ps.numpy
+            ps.pillow
+            ps.tqdm
+            ps.wand
+            ps.fonttools
+            ps.scipy
+            ps.waitress
+            ps.bottle
+            ps.diskcache
+            ps.flake8
+            ps.psutil
+            ps.pyyaml
+            ps.onnx
+            ps.packaging
+            ps.wxPython_4_2
+            ps.av
+          ];
+        in
+          (pkgs.python311.withPackages my-pkgs).env;
+
+        WAIFU_DIRECTORY="${waifu2x}";
+      }
+    );
+}
